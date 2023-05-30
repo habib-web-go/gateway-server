1e85b81e9e0b1795b7f9bea098ba78f4786af9df
amirshabanics <amirshabanics@gmail.com>
2023-05-30 12:12:58 +0330

    Imeplemt Biz Routes & Better Coding For Gateway

--- routes/auth.go
+++ routes/auth.go
@@ -1,10 +1,8 @@
 package routes
 
 import (
-	"fmt"
 	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	pb "github.com/habib-web-go/gateway-server/grpc/auth"
@@ -18,8 +16,11 @@ func addAuthRoutes(rg *gin.Engine) (*grpc.ClientConn, error) {
 	auth := rg.Group("/auth")
 	client := pb.NewAuthServiceClient(conn)
 	auth.GET("/req_pq", func(c *gin.Context) {
-		req, err := parseReqPQParams(c)
-		if err != nil {
+		var req *pb.ReqPQRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
 			return
 		}
 		res, err := client.ReqPQ(c, req)
@@ -29,12 +30,15 @@ func addAuthRoutes(rg *gin.Engine) (*grpc.ClientConn, error) {
 			})
 			return
 		}
-		c.JSON(http.StatusOK, parseReqPQResponse(res))
+		c.JSON(http.StatusOK, res)
 	})
 
 	auth.GET("/req_dh_params", func(c *gin.Context) {
-		req, err := parseReqDHParams(c)
-		if err != nil {
+		var req *pb.ReqDHParamsRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
 			return
 		}
 		res, err := client.ReqDHParams(c, req)
@@ -44,60 +48,8 @@ func addAuthRoutes(rg *gin.Engine) (*grpc.ClientConn, error) {
 			})
 			return
 		}
-		c.JSON(http.StatusOK, parseReqDHResponse(res))
+		c.JSON(http.StatusOK, res)
 	})
 
 	return conn, err
-}
-
-func parseReqPQParams(c *gin.Context) (*pb.ReqPQRequest, error) {
-	nonce := c.Param("nonce")
-	messageId, err := strconv.ParseUint(c.Param("message_id"), 10, 64)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return nil, err
-	}
-	req := &pb.ReqPQRequest{Nonce: nonce, MessageId: messageId}
-	return req, nil
-}
-
-func parseReqDHParams(c *gin.Context) (*pb.ReqDHParamsRequest, error) {
-	nonce := c.Param("nonce")
-	serverNonce := c.Param("server_nonce")
-	a := c.Param("a")
-	messageId, err := strconv.ParseUint(c.Param("message_id"), 10, 64)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return nil, err
-	}
-	req := &pb.ReqDHParamsRequest{
-		Nonce:       nonce,
-		ServerNonce: serverNonce,
-		A:           a,
-		MessageId:   messageId,
-	}
-	return req, nil
-}
-
-func parseReqPQResponse(res *pb.ReqPQResponse) gin.H {
-	return gin.H{
-		"g":            fmt.Sprint(res.G),
-		"p":            fmt.Sprint(res.P),
-		"message_id":   fmt.Sprint(res.MessageId),
-		"nonce":        fmt.Sprint(res.Nonce),
-		"server_nonce": fmt.Sprint(res.ServerNonce),
-	}
-}
-
-func parseReqDHResponse(res *pb.ReqDHParamsResponse) gin.H {
-	return gin.H{
-		"nonce":        fmt.Sprint(res.Nonce),
-		"server_nonce": fmt.Sprint(res.ServerNonce),
-		"message_id":   fmt.Sprint(res.MessageId),
-		"b":            fmt.Sprint(res.B),
-	}
 }
--- routes/biz.go
+++ routes/biz.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	pb "github.com/habib-web-go/gateway-server/grpc/biz"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -13,9 +14,41 @@ func addBizRoutes(rg *gin.Engine) (*grpc.ClientConn, error) {
 	address := os.Getenv("BIZ_ADDR")
 	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	biz := rg.Group("/auth")
+	client := pb.NewSQLServiceClient(conn)
+	biz.GET("/get_users", func(c *gin.Context) {
+		var req *pb.GetUsersRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		res, err := client.GetUsers(c, req)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, res)
+	})
 
-	biz.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "pong")
+	biz.GET("/get_users_with_sql_inject", func(c *gin.Context) {
+		var req *pb.GetUsersWithSqlInjectRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		res, err := client.GetUsersWithSqlInject(c, req)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, res)
 	})
 	return conn, err
 }
